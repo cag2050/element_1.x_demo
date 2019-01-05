a21a833141979c39cbcbec4e9f3a32042003d265
chen_anguo <cag2050@163.com>
2019-01-05 17:22:14 +0800

    add:前端分页

--- src/views/FrontendPagination.vue
+++ src/views/FrontendPagination.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <el-table :data="tableData | pagination(pageNo, pageSize)">
+        <el-table :data="tableData | paginationFilter(pageNo, pageSize)">
             <el-table-column label="ID">
                 <template slot-scope="scope">
                     {{scope.row.id}}
@@ -8,7 +8,6 @@
             </el-table-column>
         </el-table>
         <el-pagination
-            @size-change="handleSizeChange"
             @current-change="handleCurrentChange"
             :current-page="pageNo"
             :page-size="pageSize"
@@ -35,15 +34,12 @@ export default {
     mounted () {
     },
     methods: {
-        handleSizeChange (val) {
-            this.pageSize = val
-        },
         handleCurrentChange (val) {
             this.pageNo = val
         }
     },
     filters: {
-        pagination (array, pageNo, pageSize) {
+        paginationFilter (array, pageNo, pageSize) {
             let offset = (pageNo - 1) * pageSize
             let data = (offset + pageSize >= array.length) ? array.slice(offset, array.length) : array.slice(offset, offset + pageSize)
             return data
