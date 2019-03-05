bda471dcb998af5f7313ad5e82e5cff1f819d352
chen_anguo <cag2050@163.com>
2019-03-05 16:04:47 +0800

    解决：弹出dialog框时，页面滚动条总是跳到顶部

--- src/views/DialogDemo.vue
+++ src/views/DialogDemo.vue
@@ -7,8 +7,10 @@
             :visible.sync="dialogVisible"
             size="small"
             :show-close="false"
+            :lock-scroll="false"
         >
-            <span>这是一段信息</span>
+            <p style="height: 400px;">遇到过一个问题：弹出dialog框时，页面滚动条总是跳到顶部；<br/>
+            解决：添加了代码：`:lock-scroll="false"`</p>
             <img src="../assets/logo.png">
         </el-dialog>
     </div>
@@ -24,15 +26,15 @@ export default {
     methods: {
         handleClick () {
             this.dialogVisible = true
-            let scrollTop = 0
-            if (document.documentElement && document.documentElement.scrollTop) {
-                scrollTop = document.documentElement.scrollTop
-            } else if (document.body) {
-                scrollTop = document.body.scrollTop
-            }
-            console.log(scrollTop)
-            // window.scrollTo(0, scrollTop)
-            window.scrollTo(0, scrollTop - 50)
+            // let scrollTop = 0
+            // if (document.documentElement && document.documentElement.scrollTop) {
+            //     scrollTop = document.documentElement.scrollTop
+            // } else if (document.body) {
+            //     scrollTop = document.body.scrollTop
+            // }
+            // console.log(scrollTop)
+            // // window.scrollTo(0, scrollTop)
+            // window.scrollTo(0, scrollTop - 50)
         }
     }
 }
